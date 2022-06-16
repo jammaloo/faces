aa2c064dc5e1642239d3e8bc41b906d5628806fc
James Freeman <james@jammaloo.com>
2022-06-16 08:49:26 -0400

    feat: add throttle to event handler

--- main.js
+++ main.js
@@ -13,29 +13,36 @@ for (let sliceIndex = 0; sliceIndex < sliceCount; sliceIndex++) {
     container.appendChild(createSlice(imageUrl, imageBackgroundColor, startingSize, sliceIndex, sliceCount));
 }
 
+let timerId = null;
 const moveHandler = (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    const centreX = window.innerWidth / 2;
-    const centreY = window.innerHeight / 2;
-    const deltaX = (mouseX - centreX) / window.innerWidth;
-    const deltaY = (mouseY - centreY) / window.innerHeight;
-    const imageSlices = document.querySelectorAll('.slice');
-    imageSlices.forEach((slice, sliceIndex) => {
-        switch (currentMode) {
-            case 'normal':
-                slice.style.top = `${mouseY}px`;
-                slice.style.left = `${mouseX}px`;
-                break;
-            case 'fixed':
-                const sliceOffset = 1 - (sliceIndex * (1 / imageSlices.length)) * 250;
-                const deltaYOffset = deltaY * sliceOffset;
-                const deltaXOffset = deltaX * sliceOffset;
-                slice.style.top = `${centreY - deltaYOffset}px`;
-                slice.style.left = `${centreX - deltaXOffset}px`;
-                break;
-        }
-    });
+    if (timerId) {
+        return;
+    }
+    timerId = setTimeout(() => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        const centreX = window.innerWidth / 2;
+        const centreY = window.innerHeight / 2;
+        const deltaX = (mouseX - centreX) / window.innerWidth;
+        const deltaY = (mouseY - centreY) / window.innerHeight;
+        const imageSlices = document.querySelectorAll('.slice');
+        imageSlices.forEach((slice, sliceIndex) => {
+            switch (currentMode) {
+                case 'normal':
+                    slice.style.top = `${mouseY}px`;
+                    slice.style.left = `${mouseX}px`;
+                    break;
+                case 'fixed':
+                    const sliceOffset = 1 - (sliceIndex * (1 / imageSlices.length)) * 250;
+                    const deltaYOffset = deltaY * sliceOffset;
+                    const deltaXOffset = deltaX * sliceOffset;
+                    slice.style.top = `${centreY - deltaYOffset}px`;
+                    slice.style.left = `${centreX - deltaXOffset}px`;
+                    break;
+            }
+        });
+        timerId = null;
+    }, 10);
 };
 
 const changeMode = () => {
